f67553450650b6cb3da7015afa0f7ade2892cdce
Watcharachai Samkham <100463568+WatcharachaiSk@users.noreply.github.com>
2026-05-10 16:54:15 +0700

    update eslint

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,  // ปิดการตรวจสอบ ESLint ระหว่างการ build
-  },
   /* config options อื่น ๆ ที่คุณต้องการเพิ่ม */
 };
 
